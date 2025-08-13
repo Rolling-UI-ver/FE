39594c966a94073c89deefe62f7002ee3f4dac46
jaewon8405 <wo19878405@gmail.com>
2025-08-13 21:41:55 +0900

    Design: 컬러 선택창을 바탕으로 1차 UI 및 테일윈드로 적용

--- src/pages/Post/PostPage.jsx
+++ src/pages/Post/PostPage.jsx
@@ -1,36 +1,60 @@
-import './PostPage.css';
+// import './PostPage.css';
 
 export default function Postpage() {
   return (
     <>
       <header>
-        <div className="header-content">
+        <div className="max-w-[1920px] px-[360px] py-[11px] flex justify-between items-center mb-[60px]">
           <div>Rolling</div>
           <button>버튼</button>
         </div>
       </header>
       <main>
-        <div className="post-section">
-          <div className="section-input">
-            <h1>To.</h1>
+        <div className="mx-auto w-[720px] flex flex-col gap-[60px]">
+          {/* 입력 영역 */}
+          <div className="flex flex-col gap-[12px]">
+            <h1 className="text-[24px] font-bold">To.</h1>
             <input
-              className="input-name"
+              className="px-[16px] py-[12px] border border-[#d9d9d9] rounded-[8px]"
               type="text"
               placeholder="받는 사람 이름을 입력해주세요."
             />
           </div>
-          <div className="background-section">
-            <div className="backgroud-description">
-              <h2>배경화면을 선택해주세요.</h2>
-              <p>컬러를 선택하거나, 이미지를 선택할 수 있습니다.</p>
+
+          {/* 배경 선택 영역 */}
+          <div>
+            <div className="mb-[40px]">
+              <h2 className="text-[24px] font-bold">
+                배경화면을 선택해주세요.
+              </h2>
+              <p className="text-[#555555]">
+                컬러를 선택하거나, 이미지를 선택할 수 있습니다.
+              </p>
             </div>
-            <div className="select-btn">
-              <button className="btn">컬러</button>
-              <button className="btn">이미지</button>
+            <div className="mt-[40px]">
+              <div className="flex">
+                <button className="w-[122px] h-[40px] px-[16px] py-[7px] rounded-[6px] bg-[#F6F6F6]">
+                  컬러
+                </button>
+                <button className="w-[122px] h-[40px] px-[16px] py-[7px] rounded-[6px] bg-[#F6F6F6]">
+                  이미지
+                </button>
+              </div>
+
+              {/* 색상 박스들 */}
+              <div className="mt-[40px] flex gap-[16px]">
+                <div className="w-[168px] h-[168px] rounded-[8%] bg-[#ffe2ad]"></div>
+                <div className="w-[168px] h-[168px] rounded-[8%] bg-[#ecd9ff]"></div>
+                <div className="w-[168px] h-[168px] rounded-[8%] bg-[#b1e4ff]"></div>
+                <div className="w-[168px] h-[168px] rounded-[8%] bg-[#d0f5c3]"></div>
+              </div>
             </div>
           </div>
-          <div></div>
-          <button className="create-btn">생성하기</button>
+
+          {/* 생성 버튼 */}
+          <button className="w-full h-[56px] text-white bg-[#9935ff] rounded-[12px] px-[24px] py-[14px]">
+            생성하기
+          </button>
         </div>
       </main>
     </>
